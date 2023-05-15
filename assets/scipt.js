b442e37cda2050821fff40cdf99abc3525bc2bfb
valderlan jose da silva <105988303+valderlanjs@users.noreply.github.com>
2023-05-15 00:10:44 -0300

    criado o js e alterado o css

--- assets/scipt.js
+++ assets/scipt.js
@@ -2,10 +2,9 @@ $(window).load(function () {
   let body = $("body"),
     universe = $("#universe"),
     solarsys = $("#solar-system");
-
-  let int = function () {
+  let init = function () {
     body
-      .removeClass("viwe-2D opening")
+      .removeClass("view-2D opening")
       .addClass("view-3D")
       .delay(2000)
       .queue(function () {
@@ -13,19 +12,38 @@ $(window).load(function () {
         $(this).dequeue();
       });
   };
-
   let setView = function (view) {
     universe.removeClass().addClass(view);
-    $("toggle-data").click(function (e) {
-      body.toggleClass("data-open data-close");
-      e.preventDefault();
-    });
-
-    $("#toggle-controls").click(function (e) {
-      body.toggleClass("controls-open controls-close");
-      e.preventDefault();
-    });
-
-    $("#data a").click();
   };
+  $("#toggle-data").click(function (e) {
+    body.toggleClass("data-open data-close");
+    e.preventDefault();
+  });
+  $("#toggle-controls").click(function (e) {
+    body.toggleClass("controls-open controls-close");
+    e.preventDefault();
+  });
+  $("#data a").click(function (e) {
+    let ref = $(this).attr("class");
+    solarsys.removeClass().addClass(ref);
+    $(this).parent().find("a").removeClass("active");
+    $(this).addClass("active");
+    e.preventDefault();
+  });
+  $(".set-view").click(function () {
+    body.toggleClass("view-3D view-2D");
+  });
+  $(".set-zoom").click(function () {
+    body.toggleClass("zoom-large zoom-close");
+  });
+  $(".set-speed").click(function () {
+    setView("scale-stretched set-speed");
+  });
+  $(".set-size").click(function () {
+    setView("scale-s set-size");
+  });
+  $(".set-distance").click(function () {
+    setView("scale-d set-distance");
+  });
+  init();
 });
