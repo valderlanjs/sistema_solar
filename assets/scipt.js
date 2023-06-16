13894e9168257504263bdb4b645cb2b289535a74
valderlan jose da silva <105988303+valderlanjs@users.noreply.github.com>
2023-06-16 00:08:05 -0300

    add the comentary in JS

--- assets/scipt.js
+++ assets/scipt.js
@@ -1,50 +1,77 @@
+//Evento que é acionado quando a pagina é carregado por completo para executar o código dentro da função.
 $(window).load(function () {
-  let body = $("body"),
-    universe = $("#universe"),
-    solarsys = $("#solar-system");
+  //Seleciona os elementos relevantes
+  let body = $("body"), // Elemento Body
+    universe = $("#universe"), // Elemento com id "universe"
+    solarsys = $("#solar-system"); // Elemento com id "solor-system"
 
+  // Função de inicialização
   let init = function () {
     body
-      .removeClass("view-2D opening")
-      .addClass("view-3D")
-      .delay(2000)
+      .removeClass("view-2D opening") // Remove as classes view-2d e opening do body
+      .addClass("view-3D") // Adiciona a classe view-3d ao body
+      .delay(2000) // atraso de 2 segundos antes da execução do proximo comando
       .queue(function () {
-        $(this).removeClass("hide-UI").addClass("set-speed");
-        $(this).dequeue();
+        // adiciona uma função a fila de comando a serem executadas
+        $(this).removeClass("hide-UI").addClass("set-speed"); // Remove a class "hide-UI" e adiciona a classe "set-speed"
+        $(this).dequeue(); // remove o proximo comando da fila
       });
   };
   let setView = function (view) {
-    universe.removeClass().addClass(view);
+    // Define a visualização do universo
+    universe.removeClass().addClass(view); // Remove todas as classes e adiciona a classe especificada para o elemento "universe"
   };
   $("#toggle-data").click(function (e) {
-    body.toggleClass("data-open data-close");
-    e.preventDefault();
+    // Manipula o evento de clique no elemento com id "toggle-data"
+    body.toggleClass("data-open data-close"); // Alterna entre as classes "data-open" e "data-close" no <body>
+    e.preventDefault(); // Impede o comportamento padrão do link (neste caso, seguir o link)
   });
+
   $("#toggle-controls").click(function (e) {
-    body.toggleClass("controls-open controls-close");
-    e.preventDefault();
+    // Manipula o evento de clique no elemento com id "toggle-controls"
+
+    body.toggleClass("controls-open controls-close"); // Alterna entre as classes "controls-open" e "controls-close" no <body>
+    e.preventDefault(); // Impede o comportamento padrão do link (neste caso, seguir o link)
   });
+
   $("#data a").click(function (e) {
-    let ref = $(this).attr("class");
-    solarsys.removeClass().addClass(ref);
-    $(this).parent().find("a").removeClass("active");
-    $(this).addClass("active");
+    // Manipula o evento de clique em um link dentro do elemento com id "data"
+    let ref = $(this).attr("class"); // Obtém o valor da classe do link clicado
+    solarsys.removeClass().addClass(ref); // Remove todas as classes e adiciona a classe obtida para o elemento "solarsys"
+    $(this).parent().find("a").removeClass("active"); // Remove a classe "active" de todos os links dentro do elemento pai do link clicado
+    $(this).addClass("active"); // Adiciona a classe "active" ao link clicado
     e.preventDefault();
   });
+
   $(".set-view").click(function () {
-    body.toggleClass("view-3D view-2D");
+    // Manipula o evento de clique em um elemento com a classe "set-view"
+
+    body.toggleClass("view-3D view-2D"); // Alterna entre as classes "view-3D" e "view-2D" no <body>
   });
+
   $(".set-zoom").click(function () {
-    body.toggleClass("zoom-large zoom-close");
+    // Manipula o evento de clique em um elemento com a classe "set-zoom"
+
+    body.toggleClass("zoom-large zoom-close"); // Alterna entre as classes "zoom-large" e "zoom-close" no <body>
   });
+
   $(".set-speed").click(function () {
-    setView("scale-stretched set-speed");
+    // Manipula o evento de clique em um elemento com a classe ".set-speed"
+
+    setView("scale-stretched set-speed"); // Define a visualização do universo com as classes "scale-stretched" e "set-speed"
   });
+
   $(".set-size").click(function () {
-    setView("scale-s set-size");
+    // Manipula o evento de clique em um elemento com a classe ".set-size"
+
+    setView("scale-s set-size"); // Define a visualização do universo com as classes "scale-s" e "set-size"
   });
+
   $(".set-distance").click(function () {
-    setView("scale-d set-distance");
+    // Manipula o evento de clique em um elemento com a classe ".set-distance"
+
+    setView("scale-d set-distance"); // Define a visualização do universo com as classes "scale-d" e "set-distance"
   });
-  init();
+
+  init(); // Chama a função de inicialização
 });
